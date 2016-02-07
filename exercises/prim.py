d92d49b73d2fcee3a1a6932979971ce5483a03fd
sophie <sophie.n.walther@googlemail.com>
2016-02-07 10:33:07 +0100

    Done first exercise

--- exercises/prim.py
+++ exercises/prim.py
@@ -1,2 +1,24 @@
+from numpy import arange
+n0 = 2
+
 def prim(n):
-    pass
+
+    prim_array = [n0]
+
+    if n == 2:
+        return prim_array
+    else:
+        for i in arange(3, n+1):
+            write = True
+            for j in prim_array:
+                rest = i % j
+                if rest == 0:
+                    write = False
+                    break
+                elif rest != 0:
+                    write = True
+
+            if write:
+                prim_array.append(i)
+
+    return prim_array
